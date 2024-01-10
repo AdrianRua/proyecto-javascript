adaec0490879bc5d47d06d56852e22c8faf1d674
Adrian Rua <adrianamericorua@gmail.com>
2024-01-10 19:05:58 -0300

    agregado y correciones de funcion

--- primer-entrega/main.js
+++ primer-entrega/main.js
@@ -1,45 +1,40 @@
-let producto = prompt("¿Que sector de la cancha busca?");
-let precio = Number(prompt("¿Que precio paga?"));
+//Pagina de basquet
 
+let Basquet = prompt("¿Quieres saber sobre basquet?").toLowerCase();
 
-if (producto != "" && precio <=6000) {
-    alert("tenemos disponible populares por ese valor")
-} else if(producto != "" && precio > 6000 && precio < 30000){
-    alert("tenemos disponible el sector de plateas por ese valor");
-} else{
-    alert("No hay disponibilidad de palcos")
+if (Basquet != "no") {
+    alert("Genial, descubre el numero según la posicion de campo");
+    alert("¿En que posicion jugarias?")
+    seguir = '';
+    while (seguir != 'salir') {
+    seguir = funcCamiseta()
+    if (seguir == 'esc') {
+    seguir = 'salir';
+    } else if (seguir == 'reiniciar'){
+    continue
+    } else {
+        alert(seguir)
+    }
+    }
 }
 
-
-for(let numero = 1; numero <= 5; numero++){
-    let posicion = prompt("Escriba una posicion de basquet").toLowerCase()
-    while(posicion != "ESC"){
-        switch (posicion){
-            case "base":
-                alert(`En la camiseta representa el: #${numero}`);
-                break;
-
-            case "escolta":
-                alert(`En la camiseta representa el: #${numero}`);
-                break;
-
-                case "alero":
-                    alert(`En la camiseta representa el: #${numero}`);
-                    break;
-
-                case "ala pivot":
-                    alert(`En la camiseta representa el: #${numero}`);
-                    break;
-
-                        case "pivot":
-                            alert(`En la camiseta representa el: #${numero}`);
-                            break;
-        
-            default:
-                alert("No existe esa posicion");
-                break;
-        }
-
-
+function funcCamiseta() {
+    let posicion = prompt('Ingresa tu posición, "esc" para salir').toLowerCase();
+    
+    if (posicion == "base") {
+        return `En la camiseta representa el #1`
+    } else if (posicion == "escolta") {
+        return 'En la camiseta representa el #2'
+    } else if (posicion == "alero") {
+        return 'En la camiseta representa el #3'
+    } else if (posicion == "ala pivot") {
+        return `En la camiseta representa el #4`
+    } else if (posicion == "pivot") {
+        return `En la camiseta representa el #5`
+    }else if (posicion == "esc") {
+        return `esc`
+    } else {
+        alert('Esa posición no existe')
+        return 'reiniciar'
     }
 }
