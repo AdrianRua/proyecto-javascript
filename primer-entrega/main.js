e8379cafa36c95abf25ed850293a68c4e741a8a8
Adrian Rua <adrianamericorua@gmail.com>
2024-01-10 22:18:44 -0300

    arrays, objetos, funciones de orden superior

--- primer-entrega/main.js
+++ primer-entrega/main.js
@@ -38,3 +38,46 @@ function funcCamiseta() {
         return 'reiniciar'
     }
 }
+
+
+
+const jugadores = [];
+
+class Jugador{
+    constructor( nombre, edad, club, nacionalidad){
+        this.id = jugadores.length + 1;
+        this.nombre = nombre.toLowerCase();
+        this.edad = edad;
+        this.club = club.toLowerCase();
+        this.nacionalidad = nacionalidad.toLowerCase();
+    }
+}
+
+jugadores.push(new Jugador("Stephen Curry", 35, "Golden State Warriors", "USA"));
+jugadores.push(new Jugador("Lebron James", 39, " L.A Lakers", "USA"));
+jugadores.push(new Jugador("Luka Dončić", 24, "Dallas Mavericks", "Eslovenia"));
+jugadores.push(new Jugador("Kevin Durant", 35, "Phoenix Suns", "USA"));
+jugadores.push(new Jugador("James Harden", 34, "Los Angeles Clippers", "USA"));
+jugadores.push(new Jugador("Anthony Davis", 30, "L.A Lakers", "USA"));
+jugadores.push(new Jugador("Victor Wembanyama", 20, "Spurs", "Francia"));
+jugadores.push(new Jugador("Jimmy Butler", 34, "Miami Heat", "USA"));
+jugadores.push(new Jugador("Giannis Antetokounmpo",29, "Milwaukee Bucks", "Grecia"));
+
+function filtrarJugador() {
+    let edad =  Number(prompt("Ingrese edad"));
+    const result = jugadores.filter((elemento) => elemento.edad > edad);
+
+    return result;
+
+}
+
+alert("Conoce a los referentes por su rango de edad");
+
+filtrarJugador().forEach(elemento => {
+    alert(`
+    jugador: ${elemento.nombre}
+    club: ${elemento.club}
+    edad: ${elemento.edad}
+    nacionalidad: ${elemento.nacionalidad}
+    `)
+});
